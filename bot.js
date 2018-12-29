8afb2e4332554e06378fd47e7aac53a8919add6a
sohaib14 <45211148+sohaib14@users.noreply.github.com>
2018-12-29 13:02:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "528525940255490059"; // ايدي السررفر
+var channel = "528526180656218114";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
